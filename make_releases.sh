8217cacb8bb7651b9b7e4e9aa2771b8f878d9593
nemik <nemik@nemik.net>
2016-07-27 22:23:09 -0500

    * fix the run_always script to exclude variables in settings * add make script

---
+++ make_releases.sh
@@ -0,0 +1,26 @@
+#setup
+rm -rf out
+mkdir out
+cp -rf static_html out/
+
+build_os_arch ()
+{
+	OS="$1"
+	ARCH="$2"
+	rm out/presence
+	env GOOS=$OS GOARCH=$ARCH go build -o out/presence
+	cd out
+	zip -r presence_${OS}_${ARCH}.zip * presence
+	mv presence_${OS}_${ARCH}.zip ..
+	cd ..
+	echo "finished building presence_${OS}_${ARCH}.zip"
+}
+
+build_os_arch "darwin" "amd64"
+build_os_arch "linux" "386"
+build_os_arch "linux" "amd64"
+build_os_arch "linux" "arm"
+build_os_arch "linux" "arm64"
+build_os_arch "windows" "386"
+build_os_arch "windows" "amd64"
+
--- run_always.sh
+++ run_always.sh
@@ -4,6 +4,6 @@
 
 while true
 do
-	./presence -last_seen_threshold 45 -last_reading_threshold 5 -location_confidence 7 -mqtt_username=mqtt_user -mqtt_password=mqtt_password -mqtt_host=localhost:1883
+	./presence -mqtt_username=mqtt_user -mqtt_password=mqtt_password -mqtt_host=localhost:1883
 	sleep 1
 done
