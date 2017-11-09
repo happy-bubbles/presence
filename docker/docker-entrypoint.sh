3468d4293519b1a62aabdf0c5689e49aa66fce3f
Touliloup <RiRomain@users.noreply.github.com>
2017-11-09 15:19:07 +0100

    Docker container improvement (#11)
    -Replaced basis image debian with an alpine image
    (Image is now ~21MB versus ~190MB previously)
    -Removed MQTT server from the container
    -MQTT settings integrated in docker/docker-entrypoint.sh
    (Mandatory configuration of those settings)
    -README.md adjustment in consequence

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-./presence -http_host_path=0.0.0.0:5555
+./presence -http_host_path=0.0.0.0:5555 -mqtt_client_id=happy_bubbles -mqtt_host=192.168.0.XXX:1883 -mqtt_password=1234 -mqtt_username=homeassistant
