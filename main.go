040d0ff151af8e03cfea0a70fec9a15d187a2aae
nemik <nemik@nemik.net>
2018-06-20 14:39:38 -0500

    closes #18
    implements @rockinglama suggestion to show all added beacons regardless
    of if they've been seen recently.
    also implements @frenck suggestion to have the minimum threshold not be
    a surprise. Its enforcement is off by default but can be changed with a
    checkbox in the settings menu of the web UI

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"log"
 	"math"
-	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
@@ -20,9 +19,6 @@ import (
 
 	"github.com/yosssi/gmq/mqtt"
 	"github.com/yosssi/gmq/mqtt/client"
-
-	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
 )
 
 const (
@@ -41,12 +37,13 @@ const (
 // data structures
 
 type Settings struct {
-	Location_confidence  int64 `json:"location_confidence"`
-	Last_seen_threshold  int64 `json:"last_seen_threshold"`
-	Beacon_metrics_size  int   `json:"beacon_metrics_size"`
-	HA_send_interval     int64 `json:"ha_send_interval"`
-	HA_send_changes_only bool  `json:"ha_send_changes_only"`
-	RSSI_min_threshold   int64 `json:"rssi_min_threshold"`
+	Location_confidence    int64 `json:"location_confidence"`
+	Last_seen_threshold    int64 `json:"last_seen_threshold"`
+	Beacon_metrics_size    int   `json:"beacon_metrics_size"`
+	HA_send_interval       int64 `json:"ha_send_interval"`
+	HA_send_changes_only   bool  `json:"ha_send_changes_only"`
+	RSSI_min_threshold     int64 `json:"rssi_min_threshold"`
+	RSSI_enforce_threshold bool  `json:"enforce_rssi_threshold"`
 }
 
 type Incoming_json struct {
@@ -189,18 +186,14 @@ var err error
 
 var world = []byte("presence")
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
 var settings = Settings{
-	Location_confidence:  8,
-	Last_seen_threshold:  45,
-	Beacon_metrics_size:  30,
-	HA_send_interval:     5,
-	HA_send_changes_only: false,
-	RSSI_min_threshold:   -90,
+	Location_confidence:    8,
+	Last_seen_threshold:    45,
+	Beacon_metrics_size:    30,
+	HA_send_interval:       5,
+	HA_send_changes_only:   false,
+	RSSI_enforce_threshold: false,
+	RSSI_min_threshold:     -120,
 }
 
 // utility function
@@ -374,11 +367,28 @@ func getLikelyLocations(settings Settings, locations_list Locations_list, cl *cl
 	// iterate through the beacons we want to search for
 	for _, beacon := range BEACONS.Beacons {
 
+		r := HTTP_location{}
+		r.Distance = 999
+		r.Name = beacon.Name
+		r.Beacon_name = beacon.Name
+		r.Beacon_id = beacon.Beacon_id
+		r.Beacon_type = beacon.Beacon_type
+		r.HB_Battery = beacon.HB_Battery
+		r.HB_ButtonMode = beacon.HB_ButtonMode
+		r.Location = ""
+		r.Last_seen = 999
+
 		if len(beacon.beacon_metrics) == 0 {
+			http_results_lock.Lock()
+			http_results.Beacons = append(http_results.Beacons, r)
+			http_results_lock.Unlock()
 			continue
 		}
 
 		if (int64(time.Now().Unix()) - (beacon.beacon_metrics[len(beacon.beacon_metrics)-1].timestamp)) > settings.Last_seen_threshold {
+			http_results_lock.Lock()
+			http_results.Beacons = append(http_results.Beacons, r)
+			http_results_lock.Unlock()
 			continue
 		}
 
@@ -418,7 +428,7 @@ func getLikelyLocations(settings Settings, locations_list Locations_list, cl *cl
 		}
 
 		//create an http result from this
-		r := HTTP_location{}
+		r = HTTP_location{}
 		r.Distance = best_location.distance
 		r.Name = beacon.Name
 		r.Beacon_name = beacon.Name
@@ -633,7 +643,7 @@ func IncomingMQTTProcessor(updateInterval time.Duration, cl *client.Client, db *
 					// ignore this beacon if it falls below RSSI setting
 					// threshold
 
-					if int64(incoming.RSSI) < settings.RSSI_min_threshold {
+					if settings.RSSI_enforce_threshold && (int64(incoming.RSSI) < settings.RSSI_min_threshold) {
 						//fmt.Printf("rejecting rssi incoming %d < %d\n", int64(incoming.RSSI), settings.RSSI_min_threshold)
 						return
 					}
@@ -772,342 +782,4 @@ func main() {
 	if err := cli.Disconnect(); err != nil {
 		panic(err)
 	}
-}
-
-func startServer() {
-	// Set up HTTP server
-	r := mux.NewRouter()
-	r.HandleFunc("/api/results", resultsHandler)
-
-	r.HandleFunc("/api/beacons/{beacon_id}", beaconsDeleteHandler).Methods("DELETE")
-	r.HandleFunc("/api/beacons", beaconsListHandler).Methods("GET")
-	r.HandleFunc("/api/beacons", beaconsAddHandler).Methods("POST") //since beacons are hashmap, just have put and post be same thing. it'll either add or modify that entry
-	r.HandleFunc("/api/beacons", beaconsAddHandler).Methods("PUT")
-
-	r.HandleFunc("/api/latest-beacons", latestBeaconsListHandler).Methods("GET")
-
-	// what should this be?
-	// probably all the current command-line things:
-	// * mqtt connect stuff user/pass/host/port/client-id - have indicator to show it's connected to mqtt server, reconnect when needed
-	// * thresholds
-	r.HandleFunc("/api/settings", settingsListHandler).Methods("GET")
-	r.HandleFunc("/api/settings", settingsEditHandler).Methods("POST")
-
-	r.PathPrefix("/js/").Handler(http.StripPrefix("/js/", http.FileServer(http.Dir("static_html/js/"))))
-	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir("static_html/css/"))))
-	r.PathPrefix("/img/").Handler(http.StripPrefix("/img/", http.FileServer(http.Dir("static_html/img/"))))
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("static_html/")))
-
-	http.Handle("/", r)
-	http.HandleFunc("/api/beacons/ws", serveWs)
-	http.HandleFunc("/api/beacons/latest/ws", serveLatestBeaconsWs)
-	log.Fatal(http.ListenAndServe(*http_host_path_ptr, nil))
-}
-
-func resultsHandler(w http.ResponseWriter, r *http.Request) {
-	http_results_lock.RLock()
-	js, err := json.Marshal(http_results)
-	http_results_lock.RUnlock()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(js)
-}
-
-func beaconsListHandler(w http.ResponseWriter, r *http.Request) {
-	latest_list_lock.RLock()
-	js, err := json.Marshal(BEACONS)
-	latest_list_lock.RUnlock()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(js)
-}
-
-func persistBeacons() error {
-	// gob it first
-	buf := &bytes.Buffer{}
-	enc := gob.NewEncoder(buf)
-	if err := enc.Encode(BEACONS); err != nil {
-		return err
-	}
-
-	key := []byte("beacons_list")
-	// store some data
-	err = db.Update(func(tx *bolt.Tx) error {
-		bucket, err := tx.CreateBucketIfNotExists(world)
-		if err != nil {
-			return err
-		}
-
-		err = bucket.Put(key, []byte(buf.String()))
-		if err != nil {
-			return err
-		}
-		return nil
-	})
-	return nil
-}
-
-func persistButtons() error {
-	// gob it first
-	buf := &bytes.Buffer{}
-	enc := gob.NewEncoder(buf)
-	if err := enc.Encode(Buttons_list); err != nil {
-		return err
-	}
-
-	key := []byte("buttons_list")
-	// store some data
-	err = db.Update(func(tx *bolt.Tx) error {
-		bucket, err := tx.CreateBucketIfNotExists(world)
-		if err != nil {
-			return err
-		}
-
-		err = bucket.Put(key, []byte(buf.String()))
-		if err != nil {
-			return err
-		}
-		return nil
-	})
-	return nil
-}
-
-func persistSettings() error {
-	// gob it first
-	buf := &bytes.Buffer{}
-	enc := gob.NewEncoder(buf)
-	if err := enc.Encode(settings); err != nil {
-		return err
-	}
-
-	key := []byte("settings")
-	// store some data
-	err = db.Update(func(tx *bolt.Tx) error {
-		bucket, err := tx.CreateBucketIfNotExists(world)
-		if err != nil {
-			return err
-		}
-
-		err = bucket.Put(key, []byte(buf.String()))
-		if err != nil {
-			return err
-		}
-		return nil
-	})
-	return nil
-}
-
-func beaconsAddHandler(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var in_beacon Beacon
-	err = decoder.Decode(&in_beacon)
-	if err != nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-
-	//make sure name and beacon_id are present
-	if (len(strings.TrimSpace(in_beacon.Name)) == 0) || (len(strings.TrimSpace(in_beacon.Beacon_id)) == 0) {
-		http.Error(w, "name and beacon_id cannot be blank", 400)
-		return
-	}
-
-	BEACONS.Beacons[in_beacon.Beacon_id] = in_beacon
-
-	err := persistBeacons()
-	if err != nil {
-		http.Error(w, "trouble persisting beacons list, create bucket", 500)
-		return
-	}
-
-	w.Write([]byte("ok"))
-}
-
-func beaconsDeleteHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	beacon_id := vars["beacon_id"]
-	delete(BEACONS.Beacons, beacon_id)
-
-	_, ok := Buttons_list[beacon_id]
-	if ok {
-		delete(Buttons_list, beacon_id)
-	}
-
-	err := persistBeacons()
-	if err != nil {
-		http.Error(w, "trouble persisting beacons list, create bucket", 500)
-		return
-	}
-
-	w.Write([]byte("ok"))
-}
-
-func latestBeaconsListHandler(w http.ResponseWriter, r *http.Request) {
-	latest_list_lock.RLock()
-	var la = make([]Beacon, 0)
-	for _, b := range Latest_beacons_list {
-		la = append(la, b)
-	}
-	latest_list_lock.RUnlock()
-	js, err := json.Marshal(la)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(js)
-}
-
-func settingsListHandler(w http.ResponseWriter, r *http.Request) {
-	js, err := json.Marshal(settings)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(js)
-}
-
-func settingsEditHandler(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var in_settings Settings
-	err = decoder.Decode(&in_settings)
-	if err != nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-
-	//make sure values are > 0
-	if (in_settings.Location_confidence <= 0) ||
-		(in_settings.Last_seen_threshold <= 0) ||
-		(in_settings.HA_send_interval <= 0) {
-		http.Error(w, "values must be greater than 0", 400)
-		return
-	}
-
-	settings = in_settings
-
-	err := persistSettings()
-	if err != nil {
-		http.Error(w, "trouble persisting settings, create bucket", 500)
-		return
-	}
-
-	w.Write([]byte("ok"))
-}
-
-//websocket stuff
-func reader(ws *websocket.Conn) {
-	defer ws.Close()
-	ws.SetReadLimit(512)
-	ws.SetReadDeadline(time.Now().Add(pongWait))
-	ws.SetPongHandler(func(string) error { ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
-	for {
-		_, _, err := ws.ReadMessage()
-		if err != nil {
-			break
-		}
-	}
-}
-
-func writer(ws *websocket.Conn) {
-	pingTicker := time.NewTicker(pingPeriod)
-	beaconTicker := time.NewTicker(beaconPeriod)
-	defer func() {
-		pingTicker.Stop()
-		beaconTicker.Stop()
-		ws.Close()
-	}()
-	for {
-		select {
-		case <-beaconTicker.C:
-
-			http_results_lock.RLock()
-			js, err := json.Marshal(http_results)
-			http_results_lock.RUnlock()
-
-			if err != nil {
-				js = []byte("error")
-			}
-
-			ws.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := ws.WriteMessage(websocket.TextMessage, js); err != nil {
-				return
-			}
-		case <-pingTicker.C:
-			ws.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := ws.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
-				return
-			}
-		}
-	}
-}
-
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		if _, ok := err.(websocket.HandshakeError); !ok {
-			log.Println(err)
-		}
-		return
-	}
-
-	go writer(ws)
-	reader(ws)
-}
-
-func latestBeaconWriter(ws *websocket.Conn) {
-	pingTicker := time.NewTicker(pingPeriod)
-	beaconTicker := time.NewTicker(beaconPeriod)
-	defer func() {
-		pingTicker.Stop()
-		beaconTicker.Stop()
-		ws.Close()
-	}()
-	for {
-		select {
-		case <-beaconTicker.C:
-
-			latest_list_lock.RLock()
-			var la = make([]Beacon, 0)
-			for _, b := range Latest_beacons_list {
-				la = append(la, b)
-			}
-			latest_list_lock.RUnlock()
-			js, err := json.Marshal(la)
-
-			if err != nil {
-				js = []byte("error")
-			}
-
-			ws.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := ws.WriteMessage(websocket.TextMessage, js); err != nil {
-				return
-			}
-		case <-pingTicker.C:
-			ws.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := ws.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
-				return
-			}
-		}
-	}
-}
-
-func serveLatestBeaconsWs(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		if _, ok := err.(websocket.HandshakeError); !ok {
-			log.Println(err)
-		}
-		return
-	}
-
-	go latestBeaconWriter(ws)
-	reader(ws)
 }
--- static_html/js/settings.tag
+++ static_html/js/settings.tag
@@ -23,6 +23,12 @@
       </div>
       <div class="row">
         <div class="input-field col s12">
+          <input checked="{opts.enforce_rssi_threshold}" id="enforce_rssi_threshold" type="checkbox">
+          <label for="enforce_rssi_threshold" class="active">Enforce the RSSI threshold.<i class="material-icons tooltipped" data-position="right" data-tooltip="Ignore beacon transmissions below the RSSI minumum threshold below">help</i></label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
           <input value="{opts.rssi_min_threshold}" id="rssi_min_threshold" type="number">
           <label for="rssi_min_threshold" class="active">RSSI minimum threshold<i class="material-icons tooltipped" data-position="right" data-tooltip="Minimum RSSI (in dB) to include in beacon positioning calculations">help</i></label>
         </div>
@@ -78,7 +84,17 @@
 				if(data.ha_send_changes_only) {
 					$("#ha_send_changes_only").prop("checked", data.ha_send_changes_only);
 				}
-			});
+				if(data.enforce_rssi_threshold) {
+					$("#enforce_rssi_threshold").prop("checked", data.enforce_rssi_threshold);
+                }
+
+                $("#enforce_rssi_threshold").trigger("change");
+            });
+
+            $("#enforce_rssi_threshold").on("change", function(e) {
+				$("#rssi_min_threshold").prop("disabled", !($("#enforce_rssi_threshold").prop("checked")));
+				$("#rssi_min_threshold").prop("readonly", !($("#enforce_rssi_threshold").prop("checked")));
+            });
 
 			$("#settings-form").submit(function(event){
 				event.preventDefault();
@@ -89,6 +105,7 @@
 				form_data["rssi_min_threshold"] = parseInt($("#rssi_min_threshold").val());
 				form_data["ha_send_interval"] = parseInt($("#ha_send_interval").val());
 				form_data["ha_send_changes_only"] = $("#ha_send_changes_only").prop("checked");
+				form_data["enforce_rssi_threshold"] = $("#enforce_rssi_threshold").prop("checked");
 				//console.log(form_data);
 				$.ajax(
 				{
